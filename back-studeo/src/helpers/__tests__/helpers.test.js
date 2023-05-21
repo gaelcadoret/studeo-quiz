846aa99c280e4d40067f05453892ab34e0038f0d
Gael Cadoret <gaelcadoret21@gmail.com>
2023-05-21 22:53:10 +0200

    add few unit tests

---
+++ back-studeo/src/helpers/__tests__/helpers.test.js
@@ -0,0 +1,60 @@
+import { jest } from '@jest/globals';
+import { hasOwnProperty, buildResponse } from '../index';
+describe('helpers', () => {
+    describe('hasOwnProperty', () => {
+        it('should return true if object property exists', () => {
+            const person = {
+                firstName: 'Gael',
+                lastName: 'Cadoret'
+            };
+
+            expect(hasOwnProperty(person, 'firstName')).toBe(true);
+            expect(hasOwnProperty(person, 'lastName')).toBe(true);
+        });
+
+        it('should return false if object property doesn\'t exists', () => {
+            const person = {
+                firstName: 'Gael',
+                lastName: 'Cadoret'
+            };
+
+            expect(hasOwnProperty(person, 'age')).toBe(false);
+
+        });
+    });
+
+    describe('buildResponse', () => {
+
+        beforeEach(() => {
+            Date.now = jest.fn(() => 1684702214556);
+        })
+
+
+        it('should return well formated success response', () => {
+            const person = {
+                firstName: 'Gael',
+                lastName: 'Cadoret'
+            };
+
+            expect(buildResponse(true, person)).toEqual({
+                success: true,
+                data: person,
+                timestamp: 1684702214556
+            });
+
+        });
+
+        it('should return well formated error response', () => {
+            const error = {
+                message: 'Something wrong happened!',
+            };
+
+            expect(buildResponse(false, {}, error)).toEqual({
+                success: false,
+                error: error,
+                timestamp: 1684702214556
+            });
+
+        });
+    });
+});
--- back-studeo/src/helpers/index.js
+++ back-studeo/src/helpers/index.js
@@ -1,13 +1,19 @@
-export const sendErrorResponse = (res, statusCode, error) => res.status(statusCode).json({
-    success: false,
-    error,
+const getResponse = (success, data, error) => success
+    ? { data }
+    : { error };
+
+export const buildResponse = (success, data, error = undefined) => ({
+    success,
+    ...getResponse(success, data, error),
     timestamp: Date.now()
 });
 
-export const sendSuccessResponse = (res, data, statusCode = 200) => res.status(statusCode).json({
-    success: true,
-    data,
-    timestamp: Date.now()
-});
+export const sendErrorResponse = (res, statusCode, error) => res.status(statusCode).json(
+    buildResponse(false, {}, error)
+);
+
+export const sendSuccessResponse = (res, data, statusCode = 200) => res.status(statusCode).json(
+    buildResponse(true, data)
+);
 
 export const hasOwnProperty = (obj, prop) => Object.prototype.hasOwnProperty.call(obj, prop);
